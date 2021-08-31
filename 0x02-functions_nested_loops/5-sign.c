5cdd0f952b6f809b8fbafb1294cb37f954ca713e
gtfrans2re <francoisgonothitoure@gmail.com>
2021-08-31 09:12:57 -0700

    add: 5-sign.c source file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for lowercase character.
+ *
+ * @n: char type letter
+ *
+ * Return: 1, 0, -1 and prints +, 0, - if n > 0, n ==0, n < 0 respectively
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		_putchar(44);
+		_putchar(32);
+		
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		_putchar(44);
+		_putchar(32);
+
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		_putchar(44);
+		_putchar(32);
+
+		return (-1);
+	}
+}
