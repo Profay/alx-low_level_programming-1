b800aefce00f33fa49e99fe97804b16a6bf0cfbc
gtfrans2re <francoisgonothitoure@gmail.com>
2021-08-31 04:03:11 -0700

    add: 1-alphabet.c source file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students. 
+ *
+ * Function that prints the alphabet, followed by a new line.
+ *
+ * Prototype: void print_alphabet(void);
+ *
+ * You can only use _putchar twice in your code
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(c + i);
+	}
+	_putchar(10);
+}
