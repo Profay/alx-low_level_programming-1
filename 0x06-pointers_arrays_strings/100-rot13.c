8e81b20baafb9fd4f12ebf80e50cad421ab216aa
gtfrans2re <francoisgonothitoure@gmail.com>
2021-09-09 10:22:13 -0700

    modified: 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,27 @@
 #include "main.h"
+
 /**
- **rot13 - Encodes a string using rot13
- *@c: Input string
- *Return: c
+ * rot13 -  a function that encodes a string using rot13.
+ * @s: An input string to encode using rot13
+ * Return: An encode string
  */
-​
-char *rot13(char *c)
+char *rot13(char *s)
 {
-	int i, j;
-	char alp[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rotalp[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-​
-	for (i = 0; c[i] != '\0'; i++)
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		for (j = 0; alp[j] != '\0'; j++)
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if (c[i] == alp[j])
-			{
-				c[i] = rotalp[j];
-				break;
-			}
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
 		}
+		i++;
 	}
-	return (c);
+	return (s);
 }
