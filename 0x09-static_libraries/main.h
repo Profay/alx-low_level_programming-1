602cf9949d5f2958b0338c9d8457f12224e637f8
gtfrans2re <francoisgonothitoure@gmail.com>
2021-09-27 23:23:11 -0700

    add: main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#IFNDEF
-#DEFINE
+#ifndef INV_TREE_H
+#define INV_TREE_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#ENDIF
+#endif
